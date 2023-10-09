d2e5900a2fc460e12de522dba5f9cfd02db0296c
mqy224512 <mqy224512@foxmail.com>
2023-10-09 11:58:14 +0800

    finish generics、traits、quiz3

--- exercises/traits/traits3.rs
+++ exercises/traits/traits3.rs
@@ -8,10 +8,12 @@
 // Execute `rustlings hint traits3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub trait Licensed {
-    fn licensing_info(&self) -> String;
+
+    // 提供一个默认实现
+    fn licensing_info(&self) -> String {
+        "Some information".to_string()
+    }
 }
 
 struct SomeSoftware {
